9ba8b6b016650ba4c3694c5157544a5023325864
weizhang240 <weizhang240@creditease.cn>
2018-02-08 10:06:05 +0800

    打印sql语句

--- src/main/java/com/veiveid/SpringBootlearn/user/model/User.java
+++ src/main/java/com/veiveid/SpringBootlearn/user/model/User.java
@@ -18,6 +18,9 @@ public class User {
     @Column()
     private Integer age;
 
+    public User() {
+    }
+
     public User(String name, Integer age) {
         this.name = name;
         this.age = age;
